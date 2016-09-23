d83df5c8bd43cee57d9df3a39be713d1c3ed9716
Bret Gourdie <bretgourdie@gmail.com>
2016-09-23 13:06:53 -0400

    Split Tester and Logic

--- MultiplesOf3And5/MultiplesOf3And5.java
+++ MultiplesOf3And5/MultiplesOf3And5.java
@@ -1,32 +1,16 @@
 public class MultiplesOf3And5 {
-
-	public static void main(String[] args){
-		int n = 10;
-
-		MultiplesOf3And5Logic m = new MultiplesOf3And5Logic();
-
-		int sum = m.sumBelow(n);
-
-		System.out.println("Sum for n="
-		+ String.valueOf(n) 
-		+ " is " 
-		+ String.valueOf(sum));
-	}
-
-	private class MultiplesOf3And5Logic {
+	
+	public MultiplesOf3And5(){ }
+	
+	public int sumBelow(int n){
+		int sum = 0;
 		
-		public MultiplesOf3And5Logic(){ }
-
-		public int sumBelow(int n){
-			int sum = 0;
-
-			for(int ii = 1; ii < n; ii++){
-				if(ii % 3 == 0 || ii % 5 == 0){
-					sum += ii;
-				}
+		for(int ii = 1; ii < n; ii++){
+			if(ii % 3 == 0 || ii % 5 == 0){
+				sum += ii;
 			}
-
-			return sum;
 		}
+		
+		return sum;
 	}
 }
---
+++ MultiplesOf3And5/MultiplesOf3And5Tester.java
@@ -0,0 +1,18 @@
+public class MultiplesOf3And5Tester {
+	public static void main(String[] args){
+		
+		int[] testNumbers = new int[]{10, 200, 500};
+		
+		MultiplesOf3And5 m = new MultiplesOf3And5();
+		
+		for(int n : testNumbers){
+			
+			int sum = m.sumBelow(n);
+			
+			System.out.println("Sum for n="
+				+ String.valueOf(n) 
+				+ " is " 
+				+ String.valueOf(sum));
+		}
+	}
+}
