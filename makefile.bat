ea1783ed4c59095a39f4cca276f67c299c3d4eb7
Bret Gourdie <bretgourdie@gmail.com>
2016-09-23 13:16:34 -0400

    Un-hard-coded path to JDK after adjusting PATH

--- makefile.bat
+++ makefile.bat
@@ -1,1 +1,1 @@
-for /D %%i in (*) do cd "%%i" && del *.class 2>NUL && "C:\Program Files\Java\jdk1.8.0_05\bin\javac.exe" *.java && cd ..
+for /D %%i in (*) do cd "%%i" && del *.class 2>NUL && "javac.exe" *.java && cd ..
