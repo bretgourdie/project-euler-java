a93338a152789e611acd51af5e452c4176706f64
Bret Gourdie <bretgourdie@gmail.com>
2016-09-23 13:08:07 -0400

    Hide STDERR of del command

--- makefile.bat
+++ makefile.bat
@@ -1,2 +1,1 @@
-rem del *.class 2>NUL
-for /D %%i in (*) do cd "%%i" && del *.class && "C:\Program Files\Java\jdk1.8.0_05\bin\javac.exe" *.java && cd ..
+for /D %%i in (*) do cd "%%i" && del *.class 2>NUL && "C:\Program Files\Java\jdk1.8.0_05\bin\javac.exe" *.java && cd ..
