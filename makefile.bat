bf6e15540d888d2e3dab5ca6ec43bf6a4e765c6a
Bret Gourdie <bretgourdie@gmail.com>
2016-09-23 12:59:00 -0400

    makefile.bat compiles each folder

--- makefile.bat
+++ makefile.bat
@@ -1,2 +1,2 @@
-del *.class 2>NUL
-"C:\Program Files\Java\jdk1.8.0_05\bin\javac.exe" *.java
+rem del *.class 2>NUL
+for /D %%i in (*) do cd "%%i" && del *.class && "C:\Program Files\Java\jdk1.8.0_05\bin\javac.exe" *.java && cd ..
