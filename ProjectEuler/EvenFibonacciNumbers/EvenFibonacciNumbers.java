3bb0e7893a68fc9c1a1930c896fb75f0391c4cff
Bret Gourdie <bretgourdie@gmail.com>
2016-09-27 12:02:49 -0400

    Added algorithm

--- ProjectEuler/EvenFibonacciNumbers/EvenFibonacciNumbers.java
+++ ProjectEuler/EvenFibonacciNumbers/EvenFibonacciNumbers.java
@@ -1,10 +1,29 @@
-package EvenFibonacciNumbers;
+package ProjectEuler.EvenFibonacciNumbers;
 
 public class EvenFibonacciNumbers {
 	
 	public EvenFibonacciNumbers(){ }
 
 	public int findSumOfEvenValuesWithLimit(int limit){
+		int prevTerm = 1;
+		int curTerm = 2;
+		int sum = curTerm;
+
+		while(curTerm <= limit){
+			int newTerm = curTerm + prevTerm;
+			prevTerm = curTerm;
+			curTerm = newTerm;
+
+			sum += curTerm;
+
+			System.out.println("curTerm = "
+			 + String.valueOf(curTerm)
+			 + "; prevTerm = "
+			 + String.valueOf(prevTerm)
+			 + "; sum = "
+			 + String.valueOf(sum));
+		}
 		
+		return sum;
 	}
 }
