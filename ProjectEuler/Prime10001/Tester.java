1e7d514005afacdb5afbfafa1673f420027e77f8
bretgourdie <bretgourdie@gmail.com>
2016-09-29 19:12:42 -0400

    Corrected Prime10001 references

--- ProjectEuler/Prime10001/Tester.java
+++ ProjectEuler/Prime10001/Tester.java
@@ -4,7 +4,7 @@ public class Tester {
 	
 	public static void main(String[] args){
 		int[] testNums = new int[] {6};
-		10001stPrime p = new 10001stPrime();
+		Prime10001 p = new Prime10001();
 
 		for(int n : testNums){
 			int result = p.findNthPrime(n);
