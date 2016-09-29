d6e0491a26f21d94385453cf8c900fb500aa9ac2
Bret Gourdie <bretgourdie@gmail.com>
2016-09-29 13:01:26 -0400

    Fleshed out Tester

--- ProjectEuler/SmallestMultiple/Tester.java
+++ ProjectEuler/SmallestMultiple/Tester.java
@@ -1,5 +1,18 @@
+package ProjectEuler.SmallestMultiple;
+
 public class Tester {
 	public static void main(String[] args){
-		
+		int[] testNums = new int[] {10};
+		SmallestMultiple s = new SmallestMultiple();
+
+		for(int n : testNums){
+			int result = s.byRange(1, n);
+
+			System.out.println(
+				"Smallest multiple of 1 to "
+				+ String.valueOf(n)
+				+ " is: "
+				+ String.valueOf(result));
+		}
 	}
 }
