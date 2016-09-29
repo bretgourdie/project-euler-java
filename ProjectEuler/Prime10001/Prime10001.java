59ffdc0f7b378be5a914b53fda6ac70633d4995c
bretgourdie <bretgourdie@gmail.com>
2016-09-29 19:41:41 -0400

    Added algorithm

--- ProjectEuler/Prime10001/Prime10001.java
+++ ProjectEuler/Prime10001/Prime10001.java
@@ -5,6 +5,30 @@ public class Prime10001{
 	public Prime10001() { }
 
 	public int findNthPrime(int n){
-		return 0;
+		Boolean[] primeList = new Boolean[n];
+		if(primeList.length > 0){
+			primeList[0] = false;
+		}
+
+		//calculate
+		for(int ii = 2; ii <= n; ii++){
+			
+			if(primeList[ii-1] != null){
+				primeList[ii-1] = true;
+			}
+
+			for(int jj = ii+ii; jj <= n; jj += ii){
+				primeList[jj-1] = false;
+			}
+		}
+		
+		//find
+		for(int ii = primeList.length-1; ii >= 0; ii--){
+			if(primeList[ii] == true){
+				return ii+1;
+			}
+		}
+
+		return -1;
 	}
 }
