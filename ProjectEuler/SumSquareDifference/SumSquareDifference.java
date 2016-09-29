a2d9e9e33f96d7729908f721f21b37e77b1eb2b0
Bret Gourdie <bretgourdie@gmail.com>
2016-09-29 18:41:56 -0400

    Added algorithm

--- ProjectEuler/SumSquareDifference/SumSquareDifference.java
+++ ProjectEuler/SumSquareDifference/SumSquareDifference.java
@@ -5,6 +5,18 @@ public class SumSquareDifference {
 	public SumSquareDifference() { }
 
 	public int findByRange(int start, int end){
+		int sumOfSquares = 0;
+		int squareOfSum = 0;
 
+		for(int ii = start; ii <= end; ii++){
+			sumOfSquares += ii * ii;
+			squareOfSum += ii;
+		}
+
+		squareOfSum *= squareOfSum;
+
+		int difference = squareOfSum - sumOfSquares;
+
+		return difference;
 	}
 }
