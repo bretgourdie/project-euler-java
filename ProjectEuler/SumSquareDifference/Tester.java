26ef7613b4feb77984df755d0acec2f0eb4bbfac
bretgourdie <adjectivenounisadjective@gmail.com>
2016-09-29 18:38:32 -0400

    Added test condition

--- ProjectEuler/SumSquareDifference/Tester.java
+++ ProjectEuler/SumSquareDifference/Tester.java
@@ -3,6 +3,17 @@ package ProjectEuler.SumSquareDifference;
 public class Tester {
 	
 	public static void main(String[] args){
+		int[] testNums = new int[] {10};
+		SumSquareDifference s = new SumSquareDifference();
 
+		for(int n : testNums){
+			int result = s.findByRange(1, n);
+
+			System.out.println(
+					"Difference between 1 and "
+					+ String.valueOf(n)
+					+ " is: "
+					+ String.valueOf(result));
+		}
 	}
 }
