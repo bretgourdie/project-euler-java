af2575b293e17d99141077c7000f7182ae466e52
bretgourdie <bretgourdie@gmail.com>
2016-09-29 20:08:31 -0400

    Added target test condition

--- ProjectEuler/Prime10001/Tester.java
+++ ProjectEuler/Prime10001/Tester.java
@@ -3,7 +3,7 @@ package ProjectEuler.Prime10001;
 public class Tester {
 	
 	public static void main(String[] args){
-		int[] testNums = new int[] {6};
+		int[] testNums = new int[] {6, 10001};
 		Prime10001 p = new Prime10001();
 
 		for(int n : testNums){
