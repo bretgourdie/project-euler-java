84eec3c49dfb889a4ada55c3fb59d999e2bce2df
bretgourdie <bretgourdie@gmail.com>
2016-09-29 20:08:04 -0400

    Hacking the algorithm to find the Nth prime

--- ProjectEuler/Prime10001/Prime10001.java
+++ ProjectEuler/Prime10001/Prime10001.java
@@ -5,30 +5,32 @@ public class Prime10001{
 	public Prime10001() { }
 
 	public int findNthPrime(int n){
-		Boolean[] primeList = new Boolean[n];
+		Boolean[] primeList = new Boolean[n*n];
 		if(primeList.length > 0){
 			primeList[0] = false;
 		}
 
 		//calculate
-		for(int ii = 2; ii <= n; ii++){
+		for(int ii = 2; ii <= primeList.length; ii++){
 			
 			if(primeList[ii-1] == null){
 				primeList[ii-1] = true;
 			}
 
-			for(int jj = ii+ii; jj <= n; jj += ii){
+			for(int jj = ii+ii; jj <= primeList.length; jj += ii){
 				primeList[jj-1] = false;
 			}
 		}
 		
 		//find
-		for(int ii = primeList.length-1; ii >= 0; ii--){
+		int curPrime = -1;
+		for(int ii = 0; ii < primeList.length && n > 0; ii++){
 			if(primeList[ii] == true){
-				return ii+1;
+				curPrime = ii+1;
+				n--;
 			}
 		}
 
-		return -1;
+		return curPrime;
 	}
 }
