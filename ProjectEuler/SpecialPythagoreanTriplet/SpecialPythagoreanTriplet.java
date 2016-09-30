578c57367e1f1af57b8ae30c4fa84f95da8592c5
Bret Gourdie <bretgourdie@gmail.com>
2016-09-30 13:05:47 -0400

    First attempt at algorithm

--- ProjectEuler/SpecialPythagoreanTriplet/SpecialPythagoreanTriplet.java
+++ ProjectEuler/SpecialPythagoreanTriplet/SpecialPythagoreanTriplet.java
@@ -4,7 +4,21 @@ public class SpecialPythagoreanTriplet {
 	
 	public SpecialPythagoreanTriplet() { }
 
-	public int bySum(int c){
+	public int bySum(int sum){
+		for(int a = 1; a < sum; a++){
+			for(int b = 2; b < sum; b++){
+				for(int c = 3; c < sum; c++){
+					boolean isPythagorean = a < b && b < c;
+					boolean isTriplet = a*a + b*b == c*c;
+					boolean matchesSum = a + b + c == sum;
 
+					if(isPythagorean && isTriplet && matchesSum){
+						return a*b*c;
+					}
+				}
+			}
+		}
+
+		return -1;
 	}
 }
