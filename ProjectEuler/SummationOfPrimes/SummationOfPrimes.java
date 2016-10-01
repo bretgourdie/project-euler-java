aab7c410552a92a8dcaa4955052c17e7a5e7d3f3
bretgourdie <bretgourdie@gmail.com>
2016-09-30 23:49:55 -0400

    Use PrimeGenerator in SummationOfPrimes

--- ProjectEuler/SummationOfPrimes/SummationOfPrimes.java
+++ ProjectEuler/SummationOfPrimes/SummationOfPrimes.java
@@ -1,10 +1,19 @@
 package ProjectEuler.SummationOfPrimes;
 
+import ProjectEuler.PrimeGenerator;
+
 public class SummationOfPrimes {
 	
 	public SummationOfPrimes() { }
 
 	public long byLimit(int n){
+		int[] primeList = PrimeGenerator.getByLimit(n);
+		int total = 0;
 
+		for(int prime : primeList){
+			total += prime;
+		}
+
+		return total;
 	}
 }
