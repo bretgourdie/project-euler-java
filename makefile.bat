70ada889efa578aa34c764d88e7e13a2d80df68e
Bret Gourdie <bretgourdie@gmail.com>
2016-10-03 12:52:38 -0400

    Added shared library compilation

--- makefile.bat
+++ makefile.bat
@@ -1,3 +1,5 @@
 cd ProjectEuler
+del *.class 2>NUL
+for %%i in (*) do "javac.exe" -classpath "." *.java
 for /D %%i in (*) do cd "%%i" && del *.class 2>NUL && "javac.exe" -classpath "." *.java & cd ..
 cd ..
